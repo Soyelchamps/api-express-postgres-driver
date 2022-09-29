11f3f2bd0033839b31da4894a05546c8d69c9ec9
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2022-09-28 20:03:39 -0500

    Creacion y obtener mascotas

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,52 @@
 // req ---> recibe los datos
 // res ---> responde al cliente
-const controllerget = (req, res) => {
-  console.log(req.params); // Recibimos datos que llegan por la URL pero me limita la ruta
-  console.log(req.query); // Recibimos datos que llegan por la URL pero no me limita la ruta
+//req.paramas ---> Recibimos datos que llegan por la URL pero son obligatorios
+//req.query --->Recibimos los datos que llegan por la URL pero son opcionales
+//req.body ---> Recibimos los datos que llegan por el body
 
-  res.send({
-    message: "Este es el id " + req.params.productid,
+const crearMascota = (req, res) => {
+  console.log("crear mascota");
+
+  const nombre = req.body.nombre;
+  const edad = req.body.edad;
+
+  console.log(`El nombre de la mascota es ${nombre} y su edad es ${edad}`);
+
+  res.status(201).send({
+    message: "Tu mascota fue creada",
   });
 };
 
-const controllerpost = (req, res) => {
-  console.log(req.body); // Se utiliza con POST , PUT, PATCH o DELETE
-  res.send({
-    message: "usuario creado",
+const obtenerTodasMascotas = (req, res) => {
+  console.log("obtener todas las mascotas");
+  res.status(200).send({
+    data: [
+      {
+        name: "Mascota 1",
+      },
+      {
+        name: "Mascota 2",
+      },
+    ],
   });
+};
+
+const obtenerMascota = (req, res) => {
+  console.log("obtener una mascota");
+};
+
+const modificarMascota = (req, res) => {
+  console.log("modificar mascota");
+};
+
+const eliminarMascota = (req, res) => {
+  console.log("eliminar mascota");
 };
 
 module.exports = {
-  controllerget,
-  controllerpost,
+  crearMascota,
+  obtenerTodasMascotas,
+  obtenerMascota,
+  modificarMascota,
+  eliminarMascota,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const { Router } = require("express");
 const router = Router();
-const { controllerget, controllerpost } = require("../controllers");
-
-router.get("/products/:productid", controllerget);
-router.post("/products", controllerpost);
+const {
+  crearMascota,
+  obtenerTodasMascotas,
+  obtenerMascota,
+  modificarMascota,
+  eliminarMascota,
+} = require("../controllers");
+//CRUDE
+router.post("/mascotas", crearMascota);
+router.get("/mascotas", obtenerTodasMascotas);
+router.get("/mascotas/:idMascota", obtenerMascota);
+router.put("/mascotas/:idMascota", modificarMascota);
+router.delete("/mascotas/:idMascota", eliminarMascota);
 
 module.exports = router;
